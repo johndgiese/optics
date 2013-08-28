6b938d42902497e4336137e138b18d728fd5d88f
johndgiese <johndgiese@gmail.com>
2013-08-28 16:40:26 -0400

    Start writing documentation.

--- geometric/base.py
+++ geometric/base.py
@@ -1,5 +1,8 @@
 
 class PropagationException(Exception):
+    """
+    The base exception class for situations when rays stop propagating.
+    """
 
     def __init__(self, ray):
         Exception.__init__(self)
@@ -34,6 +37,28 @@ class TrappedRay(PropagationException):
 
 
 class Ray(object):
+    """
+    A class representing a single optical ray.
+
+    This Ray class keeps track of its x-position, z-position, angle, and
+    amplitude.  The angle (in radians) is relative to the positive z-axis.
+
+    Instances of this class also keep a list of references to "child rays",
+    rays that were created directly from this ray.  For example reflections
+    would be considered child rays.
+
+    Ray objects must expose a single method, "save".  This method is called by
+    optical elements every time the properties of the ray are changed.  By
+    default the save method doesn't do anything, but is exposed to define the
+    minimal interface a Ray class must implement.  An example of when the save
+    method is necessary is when one wants to trace the path of a ray through a
+    system; in order for a ray to remember its history, it must 
+
+    In general, one may be interested in modeling polarization, phase, and
+    or other quantities, so more complex ray objects are possible.  This class
+    defines the simplest Ray class; other ray models must implement at least
+    the quantities represented by this class.
+    """
 
     def __init__(self, x, th, a=1.0, z=0.0):
         self.x = x
@@ -51,6 +76,12 @@ class Ray(object):
 
 
 class Source(object):
+    """
+    An abstract base class for optical sources.
+
+    Optical sources are iterators that return rays.  The only required argument
+    is the Ray class that is used to create rays.
+    """
 
     def __init__(self, **kwargs):
         self.Ray = kwargs.pop('Ray', Ray)
@@ -63,6 +94,33 @@ class Source(object):
 
 
 class OpticalElement(object):
+    """
+    An abstract class representing an optical element.
+
+    Common optical elements are lenses, free space, prisms, apertures, etc.
+
+    All optical elements must occupy a fixed segment of the z-axis.  Optical
+    elements must implement a method, "dz", which returns its width in meters.
+    Note that the width of of an element can be zero (e.g. for a thin lens).
+    
+    Optical elements must implement a "propagate" method, which takes a
+    single ray and propagates the ray through itself.  For example, a "free
+    space" optical element would adjust the ray's z-position and x-position.
+    
+    Note that the ray's save method should be invoked every time the ray's
+    properties are adjusted, thus for a lens, the ray's save method will be
+    invoked once, but for a volume diffuser where the ray bounces several times
+    before passing through the system, the save method may be invoked many
+    hundreds of times.
+    
+    Note that because optical elements are required to fill a fixed segment of
+    the z-axis, some useful optical elements aren't possible.  For example, it
+    is impossible to have a "Bead" optical element and then place two instances
+    of the Bead element side-by-side on the x-axis.  It would be possible to
+    create a Bead element and place them side by side along the z-axis, or even
+    to create an optical element that has several beads that it keeps track of
+    internally.
+    """
 
     def propagate(self, ray):
         raise NotImplementedError
